ca86caaf0b1e2a1af4cceec191cabf1bbaebf47c
TuongLL <lamtuong2012392@gmail.com>
2023-04-18 23:02:05 +0700

    feat: Define dynamic routing for product catalog

---
+++ src/pages/collections/[productCatalog].js
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+export default function productCatalog() {
+    const router = useRouter()
+    const productCatalogType = router.query.productCatalog
+    return <h1>{productCatalogType}</h1>
+
+}
