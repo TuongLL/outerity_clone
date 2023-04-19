74957fbd1e1600355e43d32aff07f13e71c5da15
TuongLL <lamtuong2012392@gmail.com>
2023-04-19 22:40:18 +0700

    feat: Add complete header component  with logo, sign in, sign up, cart

--- src/pages/collections/[productCatalog].jsx
+++ src/pages/collections/[productCatalog].jsx
@@ -1,6 +1,10 @@
+import { Box } from "@mui/material";
 import { useRouter } from "next/router";
+import Header from "../components/Header";
 export default function productCatalog() {
     const router = useRouter()
     const productCatalogType = router.query.productCatalog
-    return <h1>{productCatalogType}</h1>
+    return <Box>
+        <Header />
+    </Box>
 }
---
+++ src/pages/components/Header.jsx
@@ -0,0 +1,136 @@
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+import Link from "next/link";
+
+
+const headerHero = [
+  {
+    iconImg:
+      "https://theme.hstatic.net/200000312481/1000699457/14/policy_icon_1.png?v=231",
+    title: "Hotline mua hàng",
+    subTitle: "Gọi ngay: 111-111-111",
+  },
+  {
+    iconImg:
+      "	https://theme.hstatic.net/200000312481/1000699457/14/policy_icon_2.png?v=231",
+    title: "Giao hàng tận nơi",
+    subTitle: "Miễn phí từ 5 sản phẩm",
+  },
+  {
+    iconImg:
+      "https://theme.hstatic.net/200000312481/1000699457/14/policy_icon_3.png?v=231",
+    title: "1 Đổi 1 nếu lỗi hàng",
+    subTitle: "Trong vòng 7 ngày",
+  },
+];
+
+function Header() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        gap: "24px",
+        justifyContent: "space-between",
+        padding: "20px 40px",
+      }}
+    >
+      <Box>
+        <Image
+          src="https://file.hstatic.net/200000312481/file/newlogoort_5ffe29c58c414ccebc2120bed119c8c0.png"
+          width={200}
+          height={40}
+        />
+      </Box>
+      {headerHero.map((item) => (
+        <Box
+          key={item}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "12px",
+          }}
+        >
+          <Image src={item.iconImg} width={25} height={25} />
+          <Box>
+            <Typography
+              sx={{
+                fontSize: "12px",
+                fontWeight: "700",
+              }}
+            >
+              {item.title}
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: "12px",
+                fontWeight: "700",
+                color: "#5f5d5d",
+              }}
+            >
+              {item.subTitle}
+            </Typography>
+          </Box>
+        </Box>
+      ))}
+      <Box display={"flex"} sx={{
+        alignItems:'center',
+        gap: '12px'
+      }}>
+        <AccountCircleIcon sx={{
+          fontSize:'30px'
+        }}/>
+        <Box sx={{
+          display:'flex',
+          flexDirection: 'column',
+        }}>
+          <Box
+            display="flex"
+            sx={{
+              gap: "6px",
+            }}
+          >
+            <Link href="/" style={{
+              textDecoration: 'none',
+              fontSize: '14px',
+              color: '#252a2b'
+            }}>Đăng nhập</Link>
+            <Typography>/</Typography>
+            <Link href="/" style={{
+              textDecoration: 'none',
+              fontSize: '14px',
+              color: '#252a2b'
+            }}>Đăng ký</Link>
+          </Box>
+          <Box sx={{
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+            <Typography sx={{
+              fontSize: '14px'
+            }}>Tài khoản của tôi</Typography>
+            <KeyboardArrowDownIcon />
+          </Box>
+        </Box>
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: '12px'
+      }}>
+        <LocalMallIcon sx={{
+          fontSize: '30px'
+        }}/>
+        <Typography sx={{
+          fontSize:'14px'
+        }}>Giỏ hàng</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default Header;
